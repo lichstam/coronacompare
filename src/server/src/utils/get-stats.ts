72e5b4721657be53bf1fdd9b8afbae41a711be0b
Martin Lichstam <martin.lichstam@gmail.com>
2020-03-21 21:44:34 +0100

    Some configs and minor fixes

--- src/server/src/utils/get-stats.ts
+++ src/server/src/utils/get-stats.ts
@@ -4,7 +4,7 @@ import superagent from 'superagent';
 const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
 
 const getStats = async () => {
-  const res = superagent.get(url);
+  const res: any = superagent.get(url);
   return csv().fromStream(res);
 };
 
