9a615d8ae0246ba2f96118ec3b1cf605985d100d
Martin Lichstam <martin.lichstam@gmail.com>
2020-03-23 10:15:04 +0100

    Add cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import cors from 'cors';
 import stats from './src/routes/stats';
 import reactApp from './react-app';
 
@@ -11,6 +12,23 @@ app.use(bodyparser.urlencoded({
   extended: true,
 }));
 
+const whitelist = [
+  'https://www.coronalation.com',
+  'https://coronalation.com',
+  'https://strong-market-269209.appspot.com',
+];
+
+const corsConfig = {
+  origin(origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsConfig));
 app.use('/dist', express.static('dist'));
 app.use('/stats', stats);
 
