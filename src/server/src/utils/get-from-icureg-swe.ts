e8a075b0dd6081a486dabdc404da6eed6efdfa99
Martin Lichstam <martin.lichstam@gmail.com>
2020-03-31 16:00:01 +0200

    Add .retry for icureg

--- src/server/src/utils/get-from-icureg-swe.ts
+++ src/server/src/utils/get-from-icureg-swe.ts
@@ -2,7 +2,7 @@ import superagent from 'superagent';
 import { sweICU } from '../../constants';
 
 const getFromICUregSwe = (payload: string) => async () => {
-  const res = await superagent.post(sweICU).send(payload);
+  const res = await superagent.post(sweICU).send(payload).retry(5);
   return JSON.parse(res.text).DetailedTable;
 };
 
