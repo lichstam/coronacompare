d75d2b0310b055e3c6c92ee210151ca160ee84aa
Martin Lichstam <martin.lichstam@gmail.com>
2020-04-05 21:20:10 +0200

    Add Deaths per day Swedish graph

---
+++ src/client/components/Country/components/Deaths/Deaths.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import BarChart from '../../../BarChart';
+
+type ScoresProps = { deaths: [] }
+
+const Scores = ({ deaths }: ScoresProps) => {
+  const totalsDailyDeaths = deaths[deaths.length - 2] || [];
+  const dateKeys = Object.keys(totalsDailyDeaths).filter((key) => key.includes('2020'));
+  const lastTwentyDays = dateKeys.slice(-20).reverse();
+  const numbersOfDeaths = lastTwentyDays.map((day) => +totalsDailyDeaths[day]);
+
+  const series = [{
+    name: 'Deaths',
+    data: numbersOfDeaths,
+    dataLabels: {
+      crop: false,
+      enabled: true,
+    },
+  }];
+
+  return (
+    <BarChart xValues={lastTwentyDays} title="New Death Cases Per Day" series={series} />
+  );
+};
+
+export default Scores;
